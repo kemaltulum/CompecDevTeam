858a07dea2206e228da71d3ddc331c63f4f4de3f
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2018-02-27 18:12:22 +0300

    Extend scrapper to scrap coordinates etc.

--- web_scrapping/scrapper.py
+++ web_scrapping/scrapper.py
@@ -7,16 +7,25 @@ def getItemList(res : str) -> list:
   return re.findall(r"<a title=\"(.+)\" class=\"overlay-link\" href=\"(.+)\"></a>", res)
 
 def getItemProps(res : str) -> dict:
-  try:
-    props = {}
-    props['price']    = re.findall(r"<li class=\"price-line clearfix\">\s*<span>(.+)<\/span>\s*<\/li>", res)[0]
-    props['location'] = "/".join(re.findall(r"<a href='.+?listeleme'>(.+?)<\/a>\/?", res)[:3])
-    props['numroom']  = re.findall(r"<span><span>Oda \+ Salon<\/span>(.+?)<\/span>", res)[0]
-    props['m2']       = re.findall(r"<span><span>Metrekare<\/span>(.+?)<\/span>", res)[0]
-    props['age']      = re.findall(r"<span><span>Bina Ya..<\/span>(.+?)<\/span>", res)[0]
-    return props
-  except Exception as e:
-    print('Caught exception while trying to parse page:', e)
+  props_list = {
+    'price':r"<li class=\"price-line clearfix\">\s*<span>(.+)<\/span>\s*<\/li>",
+    'coordinates':r"<input id=\"hdnRealtyCoordinates\" name=\"hdnRealtyCoordinates\" type=\"hidden\" value=\"(.+?)\" \/>",
+    'numroom':r"<span><span>Oda \+ Salon<\/span>(.+?)<\/span>",
+    'm2':r"<span><span>Metrekare<\/span>(.+?)<\/span>",
+    'age':r"<span><span>Bina Ya..<\/span>(.+?)<\/span>",
+    'heating':r"<span><span>Is.nma Tipi<\/span>(.+?)<\/span>",
+    'floor':r"<span>Bulundu.u Kat<\/span><span>(.+?)<\/span>",
+    'furnished':r"<span>E.yal.<\/span><span>(.+?)<\/span>"
+  }
+  props = {}
+  for (key, ptn) in props_list.items():
+    try:
+      props[key] = re.findall(ptn, res)[0]
+    except Exception as e:
+      print('Caught exception while trying to parse', key, ':', e)
+      props[key] = 'UNK'
+  props['location'] = "/".join(re.findall(r"<a href='.+?listeleme'>(.+?)<\/a>\/?", res)[:3])
+  return props
 
 def dictToCSV(d : dict) -> str:
   s = ""
@@ -31,14 +40,21 @@ url = "https://www.hurriyetemlak.com/konut-satilik/istanbul-sariyer-rumeli-hisar
 f = open("database.txt", "a")
 pageStart = int(input("Which page to start from : "))
 itemStart = int(input("Which item to start from (in the starting page) : "))
-for i in range(pageStart, 10):
+for i in range(pageStart, 50):
   print('Page', i)
   items = getItemList(r.get(url, headers={'User-Agent' : goodUA}, params={'page':str(i)}).text)
   for (itemid, item) in enumerate(items[itemStart:], itemStart):
     print('Page', i, 'item', itemid)
     print(item)
     link = "https://www.hurriyetemlak.com" + item[1]
-    ress = getItemProps(r.get(link, headers={'User-Agent' : goodUA}).text)
+    while True:
+      try:
+        ress = getItemProps(r.get(link, headers={'User-Agent' : goodUA}).text)
+      except Exception as e:
+        print('Caught exception', e)
+        continue
+      else:
+        break
     if ress != None:
       csvres=dictToCSV(ress)
       print(csvres)
